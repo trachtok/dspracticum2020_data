554c92a86bd5a31cd97ede9e4df340373385aff3
Karolina Trachtova <422653@mail.muni.cz>
2020-11-08 21:37:53 +0100

    adjusted index.js once again

--- docs/index.js
+++ docs/index.js
@@ -26,10 +26,10 @@ async function app() {
   var pred = document.getElementById('pred');
   if (p_cat < 0.5) {
       prob = ((1-p_cat)*100).toFixed(2);
-      pred.innerHTML = "<b>Batman</b> (probability=".concat(prob, "%)");
+      pred.innerHTML = "<b>Superman</b> (probability=".concat(prob, "%)");
   } else {
     prob = (p_cat*100).toFixed(2);
-    pred.innerHTML = "<b>Superman</b> (probability=".concat(prob, "%)");
+    pred.innerHTML = "<b>Batman</b> (probability=".concat(prob, "%)");
   }
   /// YOUR CODE ENDS HERE
 
