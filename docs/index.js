f9f6571a8d803ae31d6e9d0f57e0fcf3388b3eee
Karolina Trachtova <422653@mail.muni.cz>
2020-11-08 15:23:03 +0100

    modified index.js

--- docs/index.js
+++ docs/index.js
@@ -12,28 +12,28 @@ async function app() {
   console.log('Loading mobilenet..');
 
   // Load the model.
-  net = await tf.loadLayersModel('https://raw.githubusercontent.com/simecek/dspracticum2020/master/docs/export_model/model.json');
+  net = await tf.loadLayersModel('https://github.com/trachtok/dspracticum2020_data/blob/main/docs/export_model/model.json');
   console.log('Successfully loaded model');
 
   // Make a prediction through the model on our image.
   const imgEl = document.getElementById('img');
   const result = await net.predict(preprocessImage(imgEl));
-  const p_cat = result.dataSync()[0];
+  const p_bat = result.dataSync()[0];
   console.log('Prediction done');
 
   // For the assignment, change this
   // YOUR CODE STARTS HERE
   var pred = document.getElementById('pred');
-  if (p_cat < 0.5) {
-      prob = ((1-p_cat)*100).toFixed(2);
-      pred.innerHTML = "<b>Dog</b> (probability=".concat(prob, "%)");
+  if (p_bat < 0.5) {
+      prob = ((1-p_bat)*100).toFixed(2);
+      pred.innerHTML = "<b>Batman</b> (probability=".concat(prob, "%)");
   } else {
-    prob = (p_cat*100).toFixed(2);
-    pred.innerHTML = "<b>Cat</b> (probability=".concat(prob, "%)");
+    prob = (p_bat*100).toFixed(2);
+    pred.innerHTML = "<b>Superman</b> (probability=".concat(prob, "%)");
   }
   /// YOUR CODE ENDS HERE
 
-  return(p_cat);
+  return(p_bat);
 }
 
 app();
